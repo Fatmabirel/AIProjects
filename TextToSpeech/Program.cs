93d8a760b433343eae49e3d65f5f85f028765718
fatma <fatma@DESKTOP-URA16QH>
2025-09-25 20:34:59 +0300

    Implements System.Speech to Text to Speech

---
+++ TextToSpeech/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Speech.Synthesis;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+        speechSynthesizer.Volume = 100; // max volume
+        speechSynthesizer.Rate = 0; // normal speech speed
+
+        Console.Write("Enter text to convert to speech (or leave empty to exit): ");
+        string textToSpeak = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(textToSpeak))
+            return;
+        speechSynthesizer.Speak(textToSpeak);
+        Console.ReadLine();
+    }
+}
